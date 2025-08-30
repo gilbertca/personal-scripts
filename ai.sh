e515bfd1ef9545b796c20667639629b93866e126
Chris Gilbert <chrisagilbert.cg@gmail.com>
2025-08-30 13:25:34 -0400

    Updated changes, still doesn't work.

--- ai.sh
+++ ai.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
 # @gilbertca - 2025-08-30
 # A simple utility to screenshot and send an area of the screen to an LLM powered by ollama.
-images=$(slurp|grim -t png -l 0 -g - -|base64 -w 0 -)
-vared -p "Model to user> " -c model
+
+# Model name in ollama:
+vared -p "Model to use> " -c model
+# URL to the server:
 vared -p "URL to query> " -c url
+# User message:
 vared -p "User message to LLM> " -c message
-curl --url ${url} -X POST --json "{
-	\"model\": \"${model}\",
-	\"stream\": false,
-	\"messages\": [
-	{\"role\": \"user\", \"content\":\"${message}\", \"images\": [\"${images}\"]}
-	]
-}"
+
+# `slurp` grabs the screen area, writes to stdout
+# `grim` grabs screen area from stdin, writes the PNG data to stdout
+# `base` grabs PNG data from stdin, writes encoded base64 to stdout
+# `aijsonify.py` grabs the image from stdin and the cmdline args, writes the response from the API to stdout
+slurp|grim -t png -l 0 -g - -|base64 -w 0 -|python3 aijsonify.py --url=$url --model=$model --message=$message
---
+++ aijsonify.py
@@ -0,0 +1,51 @@
+"""
+Reads a base64-encoded image from stdin, calls the RESTful API defined by the arguments
+
+aijsonify.py
+@gilbertca - 2025-08-30
+"""
+from argparse import ArgumentParser
+from json import dumps
+from sys import stdin
+from sys import stdout
+
+from requests import post
+
+# TODO: error codes
+def aijsonify(model: str, url: str, message: str, stream: bool=False) -> None:
+    """`aijsonify` creates a JSON object, sends a request to the **url**, and prints the output to `stdout`.
+
+    Arguments:
+        **model**: the name of the model
+        **url**: the url of the API
+        **message**: the 'user' message to the model
+
+    Returns:
+        None
+    """
+    # Read from stdin to get the b64 image:
+    b64image_list = stdin.readlines()
+
+    # Format and send request:
+    request_data = {
+        "model": model,
+        "messages": {
+            "role": "user",
+            "content": message,
+            "images": b64image_list,
+        },
+        "stream": stream,
+    }
+    response = post(url=url, data=dumps(request_data))
+
+    # Handle output:
+    print(response)
+
+if __name__=="__main__":
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("--url", type=str, required=True)
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--message", type=str, required=True)
+    args = vars(parser.parse_args()) # Convert `Namespace` to `dict`
+
+    aijsonify(**args)
