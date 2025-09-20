05c7924444950d3c539e0810c59e83ad901c945e
Chris Gilbert <chrisagilbert.cg@gmail.com>
2025-09-20 09:48:41 -0400

    Updated system setup.

--- setup-debian.sh
+++ setup-debian.sh
@@ -7,7 +7,6 @@ if [[ -z $PASSWORD ]] ; then
         return 1;
 fi
 
-# cd to home directory:
 cd $HOME;
 
 # Download apps and libraries:
@@ -16,7 +15,7 @@ cd $HOME;
 sudo apt-get update -y;
 sudo apt-get upgrade -y;
 sudo apt-get full-upgrade -y;
-sudo apt-get install ncdu thunar javacc xwayland pipewire-audio flatpak build-essential curl fuzzel git lynx zsh vim sway tree htop -y;
+sudo apt-get install ncdu thunar javacc xwayland pipewire-audio flatpak build-essential curl fuzzel git lynx zsh vim sway tree htop fonts-ibm-plex wayvnc wtype wl-clipboard grim slurp zip -y;
 # Flatpak:
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 # Brave Browser:
@@ -35,7 +34,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo $PASSWORD|chsh -s /bin/zsh; # --unattended flag disables chsh, so it must be done manually
 # Install ollama: (TODO: set /etc/systemd/system/ollama.service
 #                       to automatically include `Environment=OLLAMA_MODELS=/home/ollama`)
-curl -fsSL https://ollama.com/install.sh | sh;
+#curl -fsSL https://ollama.com/install.sh | sh;
 # Install zellij (compiled in background) (TODO:
 #                               script reboots at the end, defeating the purpose)
 #git clone https://github.com/zellij-org/zellij;
@@ -46,39 +45,19 @@ curl -fsSL https://ollama.com/install.sh | sh;
 # Setup SSH:
 ssh-keygen -t ed25519 -N "" -f "$HOME/.ssh/id_ed25519";
 
-# Add configurations to .zshrc:
+# Copy and setup config
 #
-# Editor = vim:
-echo """
-export EDITOR=vim
-""">>$HOME/.zshrc;
-# Start sway on tty1:
-echo """
-if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ] ; then
-    exec sway
-fi
-""">>$HOME/.zshrc;
-
-# Add configuration to .config/sway/config
-#
-# Create config if it doesn't exist:
-if [[ ! -e $HOME/.config/sway/config ]] ; then
-        # mkdir ignores existing dirs, so we can safely 'create' them all
-        mkdir $HOME/.config;
-        mkdir $HOME/.config/sway;
-        cp /etc/sway/config $HOME/.config/sway/;
-fi
-# Set magenta focus, remove titlebars:
-echo """
-default_border pixel 1
-client.focused #FF00FF #FF00FF #000000
-""">>$HOME/.config/sway/config;
-# Set wallpaper and display settings:
-# Update workspace keys:
-# Set volume:
-echo """
-bindsym XF86AudioRaiseVolume exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-bindsym XF86AudioLowerVolume exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-bindsym XF86AudioMute exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-""">>$HOME/.config/sway/config;
-~                                    
+# Download Git repo:
+cd $HOME
+mkdir $HOME/.config
+git clone https://github.com/gilbertca/personal-scripts
+cd personal-scripts
+# ZSH and VIMRC:
+cp vimrc $HOME/.vimrc
+cp zshrc $HOME/.zshrc
+# Sway:
+mkdir $HOME/.config/sway
+cp swayconfig $HOME/.config/sway/config
+# Foot:
+mkdir $HOME/.config/foot
+cp foot.ini $HOME/.config/foot/foot.ini
