ef818d802a2824b318b63d253a39b590751bfd76
Chris Gilbert <chrisagilbert.cg@gmail.com>
2025-12-30 13:44:59 -0500

    Updated status bar

--- status_bar.sh
+++ status_bar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(cat /sys/class/power_supply/BAT0/status)" Battery:"$(cat /sys/class/power_supply/BAT0/capacity)"% "$(date --rfc-3339=seconds)"
+echo "$(wpctl get-volume @DEFAULT_SINK@)" "$(cat /sys/class/power_supply/BAT0/status)" Battery:"$(cat /sys/class/power_supply/BAT0/capacity)"% "$(date --rfc-3339=seconds)"
